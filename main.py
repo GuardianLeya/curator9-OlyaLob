90752c083ba974e01ebcc9bfd58ce379c566e9d1
GuardianLeya <lizik.kuznetsova@gmail.com>
2023-11-29 00:44:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import telebot
+
+bot = telebot.Telebot('6846219544:AAH0neij7meKyG07s_QJ4y7eZJLCy5NgbKU')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет! Смотри, что я могу!')
+
+
+@bot.message_handler(commands=['мяу'])
+def main(message):
+    bot.send_message(message.chat.id, 'Мяу-мяу!\n Не знал, что ты знаешь кошачий')
+
+
+@bot.message_handler(commands=['*ccc*'])
+def main(message):
+    bot.send_message(message.chat.id, '*Ссссс*\n Неожиданная практика парселтанга', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['предсказание'])
+def main(message):
+    bot.send_message(message.chat.id, 'Завтра тебя ждет чудесный день!')
+
+
+@bot.message_handler(commands=['ссылка'])
+def main(message):
+    bot.send_message(message.chat.id, '[тык на ссылку](https://t.me/homeisarefuge/)')
+
+
+bot.infinity_polling()
